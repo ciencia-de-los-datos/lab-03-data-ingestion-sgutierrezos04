e847d331dddfd71d7f97e561294987a927c9df9b
Samuel Gutierrez <sgutierrezos@unal.edu.co>
2024-04-05 11:35:16 -0500

    1er envio

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,37 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
-
+from io import StringIO
+import re
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    with open("clusters_report.txt") as fp:
 
+        columns1 = re.sub(r"\s{2,}", "\t", fp.readline().strip()).split("\t")
+        columns2 = re.sub(r"\s{2,}", "\t", fp.readline()).split("\t")
+        fp.readline()
+        fp.readline()
+    columns = []
+    for i in range(len(columns1)):
+        if len(columns2) > i:
+            columns.append(str(columns1[i] + " " + columns2[i]).strip())
+        else:
+            columns.append(columns1[i])
+    CLUSTER, CANTIDAD, PORCENTAJE, PRINCIPALES = [
+        col.lower().replace(" ", "_") for col in columns]
+    df = pd.read_fwf("clusters_report.txt",  skiprows=4, header=None)
+    df.columns = [CLUSTER, CANTIDAD, PORCENTAJE, PRINCIPALES]
+
+    df = df.ffill()
+
+    df[PORCENTAJE] = df[PORCENTAJE].str.replace(
+        ",", ".").str.replace(" %", "").astype(float)
+
+    df = df.groupby([CLUSTER, CANTIDAD, PORCENTAJE])[
+        PRINCIPALES].agg(lambda x: x).reset_index(name=PRINCIPALES)
+
+    df[PRINCIPALES] = df[PRINCIPALES].str.join(" ").str.replace(
+        r"\s{2,}", " ", regex=True).str.replace(".", "").str.strip()
+    
     return df
